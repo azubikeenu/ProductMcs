9c4c2613c79cf2745f9e27d7757d2f1fc1566066
Enu Azubike <enuazubike88@gmail.com>
2023-01-29 08:50:12 +0100

    added validation rules to the Dto

--- src/main/java/com/azubike/ellpisis/productmcs/dto/ProductDto.java
+++ src/main/java/com/azubike/ellpisis/productmcs/dto/ProductDto.java
@@ -5,11 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @NoArgsConstructor
 public class ProductDto {
   @With private String id;
+  @NotBlank(message = "description cannot be blank")
   private String description;
+  @NotBlank(message = "price cannot be blank")
   private Double price;
 }
